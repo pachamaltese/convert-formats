1b73575eeb6111590e2fabc09d3a82c3a881c85a
Pachá <mvargas@dcc.uchile.cl>
2019-11-23 11:09:14 -0300

    removes old references

--- server.R
+++ server.R
@@ -1,10 +1,3 @@
-if (!require('pacman')) install.packages('pacman')
-p_load(shiny,shinyjs,rio,data.table,jsonlite,jsonlite,dplyr,stringr,tools)
-
-# By default, the file size limit is 5MB. It can be changed by
-# setting this option. Here we'll raise limit to 20MB.
-options(shiny.maxRequestSize = 20*1024^2)
-
 function(input, output, session) {
   input_data <- reactive({
     uploadedfile <- input$file
--- ui.R
+++ ui.R
@@ -1,14 +1,7 @@
-if (!require('pacman')) install.packages('pacman')
-p_load(shiny,shinyjs,tools)
-
 fluidPage(
   useShinyjs(),
-  title = 'Datawheel File Converter',
-  tags$head(tags$link(rel='shortcut icon', href='http://pacha.datawheel.us/img/favicon.ico')),
-  tags$head(includeScript('www/google-analytics.js')),
-  #theme = shinytheme('flatly'),
-  theme = 'theme.css',
-  titlePanel(title = div(img(src='title.svg'), style='text-align: center;')),
+  theme = shinytheme('flatly'),
+  titlePanel('File Converter'),
   fluidRow(
     column(6, h3('Input')),
     column(6, h3('Output'))
@@ -38,8 +31,7 @@ fluidPage(
                        p('At the moment this tool supports csv, tsv, xls, xlsx, json, sav and dta.'),
                        p('Be wise and don\'t use this if doing so implies violating your company policies. We are not responsible for the use you give to this. Be warned.')),
       hr(),
-      print('\u00a9 Datawheel, 2017. Released under MIT License.'),
-      print('License and code:'), a('https://github.com/pachamaltese/convert-formats')
+      p('AGPL-3.0 license. See license and code at:'), a('https://github.com/pachamaltese/convert-formats')
     )),
     column(6,wellPanel(
       radioButtons(
@@ -51,7 +43,7 @@ fluidPage(
       conditionalPanel(condition = 'input.fileout == "json"', p(strong('Advanced json options'))),
       conditionalPanel(condition = 'input.fileout == "json"', checkboxInput('minify', 'Minify the output (leave marked if unsure)', TRUE)),
       hidden(div(id = "loading_page",
-                 div(img(src='http://pacha.datawheel.us/img/loading_small.gif'), p('') ,p('Datawheeling, please wait...'), align = 'center')
+                 div(img(src = "img/loading_icon.gif", width = "100"), p('') ,p('Processing, please wait...'), align = 'center')
       )),
       div(id = "show_preview", conditionalPanel(condition = 'output.preview', h4('Preview'), tableOutput('preview'), downloadButton('download_data', 'Download')))
     ))
